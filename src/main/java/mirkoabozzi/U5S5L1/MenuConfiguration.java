85b26b5f46ab0a3a4f5a18aad8524f5b205e3e01
Mirko Abozzi <mirko.abozzi@gmail.com>
2024-09-02 16:22:25 +0200

    consegna

---
+++ src/main/java/mirkoabozzi/U5S5L1/MenuConfiguration.java
@@ -0,0 +1,48 @@
+package mirkoabozzi.U5S5L1;
+
+import mirkoabozzi.U5S5L1.entities.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class MenuConfiguration {
+
+    @Bean
+    public Topping getToppingProsciutto() {
+        return new Topping("prosciutto");
+    }
+
+    @Bean
+    public Topping getToppingPatatine() {
+        return new Topping("patatine");
+    }
+
+    @Bean
+    public Pizza getPizzaProsciutto() {
+        return new Pizza("Pizza Prosciutto", 500, 8.00, getToppingProsciutto());
+    }
+
+
+    @Bean
+    public Pizza getPizzaPatatine() {
+        return new Pizza("Pizza Patatine", 600, 9.00, getToppingPatatine());
+    }
+
+    @Bean
+    public Drink getDrinkCocaCola() {
+        return new Drink("Coca Cola", 20, 1.00, 0.33);
+    }
+
+    @Bean
+    public Drink getBeer() {
+        return new Drink("Ichnusa", 80, 1, 0.33);
+    }
+
+    @Bean
+    public Menu getMenu(List<Product> productList) {
+        return new Menu(productList);
+    }
+
+}
--- src/main/java/mirkoabozzi/U5S5L1/U5S5L1Application.java
+++ src/main/java/mirkoabozzi/U5S5L1/U5S5L1Application.java
@@ -1,13 +1,44 @@
 package mirkoabozzi.U5S5L1;
 
+import mirkoabozzi.U5S5L1.entities.Drink;
+import mirkoabozzi.U5S5L1.entities.Menu;
+import mirkoabozzi.U5S5L1.entities.Pizza;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class U5S5L1Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(U5S5L1Application.class, args);
-	}
+    public static void main(String[] args) {
+
+        SpringApplication.run(U5S5L1Application.class, args);
+
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(U5S5L1Application.class);
+
+//        Topping topping = (Topping) ctx.getBean("getToppingProsciutto");
+//        Topping topping1 = (Topping) ctx.getBean("getToppingPatatine");
+//
+//        System.out.println(topping);
+//        System.out.println(topping1);
+
+        Pizza pizza = (Pizza) ctx.getBean("getPizzaProsciutto");
+        Pizza pizza1 = (Pizza) ctx.getBean("getPizzaPatatine");
+
+        System.out.println(pizza);
+        System.out.println(pizza1);
+
+        Drink drink = (Drink) ctx.getBean("getDrinkCocaCola");
+        Drink drink2 = (Drink) ctx.getBean("getBeer");
+
+        System.out.println(drink);
+        System.out.println(drink2);
+
+
+        Menu menu = ctx.getBean(Menu.class);
+
+        System.out.println(menu);
+
+    }
 
 }
