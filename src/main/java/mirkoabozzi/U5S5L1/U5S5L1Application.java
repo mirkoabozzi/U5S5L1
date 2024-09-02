2f293405e258c27d676496153fd44fe9c1a529cc
Mirko Abozzi <mirko.abozzi@gmail.com>
2024-09-02 13:43:53 +0200

    Initial commit

---
+++ src/main/java/mirkoabozzi/U5S5L1/U5S5L1Application.java
@@ -0,0 +1,13 @@
+package mirkoabozzi.U5S5L1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5S5L1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5S5L1Application.class, args);
+	}
+
+}
